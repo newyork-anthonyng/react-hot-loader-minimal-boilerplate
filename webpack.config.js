063041304bbb8c7c9e02f5d7f9f646896809a0ec
Anthony Ng <newyork.anthonyng@gmail.com>
2018-02-20 21:29:02 -0800

    Add webpack-hot-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 
 module.exports = {
   devtool: 'source-map',
   entry: {
     'app': [
+      'webpack-hot-middleware/client',
       'babel-polyfill',
       'react-hot-loader/patch',
       './src/index'
@@ -12,11 +14,19 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./template.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
